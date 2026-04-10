12504b329ef9d70def02b6a4e0dec602b00b6e38
rayen raddaoui <rayenraddaoui59@gmail.com>
2026-04-10 10:42:55 +0100

    correction 2 pour deployement

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,8 @@
 import Formulaire from "../compante/Formulaire"
 import './App.css'
 import './index.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Inscrir from "../compante/Inscrir"
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Inscrir from "../compante/Inscrir.tsx"
 import Todo from "../compante/Todo"
 import Acceuil from "../compante/Acceuil"
 import Calculatrice from "../compante/calculatrice"
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <>
-     <BrowserRouter basename="/proj">
+     <HashRouter>
      <Routes>
       <Route path="/" element={<Formulaire/>}/>
       <Route path="/about" element={<Inscrir/>}/>
@@ -19,7 +19,7 @@ function App() {
       <Route path="/acceuil" element={<Acceuil/>}/>
       <Route path="/calculatrice" element={<Calculatrice/>}/>
      </Routes>
-     </BrowserRouter>
+     </HashRouter>
     </>
   )
 }
